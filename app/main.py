bf650f79866b0030d0f6219283a75cbbf3b36a71
r89 <r89@r89desktop>
2021-03-30 16:52:25 +0600

    Update main.py and test

--- app/main.py
+++ app/main.py
@@ -9,10 +9,55 @@ DBPASSWORD = os.environ.get('DBPASSWORD')
 DBUSER = os.environ.get('DBUSER')
 
 
+class Block(ObjectType):
+    block_num = Int()
+    block_id = Int()
+
+
+class Forestry(ObjectType):
+    forestry_ru = String()
+    forestry_en = String()
+    forestry_id = Int()
+    block_list = List(Block)
+
+    def resolve_block_list(self, info):
+        block_list_query = """SELECT block_num, gid FROM forest.block WHERE forestry_id={}""".format(
+            self.forestry_id)
+        conn = psycopg2.connect(dbname='forest_bd_work', user=DBUSER,
+                                password=DBPASSWORD, host='192.168.31.177')
+        cur = conn.cursor()
+        cur.execute(block_list_query)
+        result = cur.fetchall()
+        conn.commit()
+        cur.close()
+        resp = []
+        for block in result:
+            resp.append(
+                Block(block_num=block[0], block_id=block[1]))
+        return resp
+
+
 class Leshoz(ObjectType):
     leshoz_ru = String()
     leshoz_en = String()
     leshoz_id = Int()
+    forestry_list = List(Forestry)
+
+    def resolve_forestry_list(self, info):
+        forestry_list_query = """SELECT forestry_ru, forestry_en, gid FROM forest.forestry WHERE leshoz_id={}""".format(
+            self.leshoz_id)
+        conn = psycopg2.connect(dbname='forest_bd_work', user=DBUSER,
+                                password=DBPASSWORD, host='192.168.31.177')
+        cur = conn.cursor()
+        cur.execute(forestry_list_query)
+        result = cur.fetchall()
+        conn.commit()
+        cur.close()
+        resp = []
+        for forestry in result:
+            resp.append(
+                Forestry(forestry_ru=forestry[0], forestry_en=forestry[1], forestry_id=forestry[2]))
+        return resp
 
 
 class Oblast(ObjectType):
@@ -22,8 +67,6 @@ class Oblast(ObjectType):
     leshoz_list = List(Leshoz)
 
     def resolve_leshoz_list(self, info):
-        print(self.oblast_id)
-        print('resole')
         leshoz_list_query = """SELECT leshoz_ru, leshoz_en, leshoz_id FROM forest.leshoz WHERE oblast_id={}""".format(
             self.oblast_id)
         conn = psycopg2.connect(dbname='forest_bd_work', user=DBUSER,
@@ -34,9 +77,7 @@ class Oblast(ObjectType):
         conn.commit()
         cur.close()
         resp = []
-        print(result)
         for leshoz in result:
-            print(leshoz)
             resp.append(
                 Leshoz(leshoz_ru=leshoz[0], leshoz_en=leshoz[1], leshoz_id=leshoz[2]))
         return resp
@@ -56,7 +97,6 @@ class Query(ObjectType):
         cur.close()
         resp = []
         for oblast in result:
-            print(oblast)
             resp.append(
                 Oblast(oblast_ru=oblast[0], oblast_en=oblast[1], oblast_id=oblast[2]))
         return resp
